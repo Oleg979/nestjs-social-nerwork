a0a4045a993bdc2386bd3ddfa07b9bb6cd02ea7e
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2020-04-19 12:09:18 +0400

    Update post.schema.ts

--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose';
 export const PostSchema = new mongoose.Schema({
   title: String,
   text: String,
-  userId: String,
-  likes: Number
+  username: String,
+  likes: String[],
+  creationDate: String
 });
